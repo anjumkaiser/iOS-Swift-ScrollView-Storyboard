308527dd3d4d827b92f1e7e69852cf0bad3886c3
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-04-23 20:17:13 +0500

    Added scrolling

--- ScrollView Storyboard/ViewController.swift
+++ ScrollView Storyboard/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var contentWidth: CGFloat = 0
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            let newWidth: CGFloat = 150
+            let newHeight: CGFloat = 150
+            let newX: CGFloat = view.frame.midX + view.frame.size.width * CGFloat(x) - 75
+            let newY: CGFloat = ( view.frame.size.height / 2 ) - 75
+            
+            contentWidth += newX
+
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
 
